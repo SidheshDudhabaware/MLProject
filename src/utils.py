6667aede750c15fd8b40fec16ec70b088e42f285
Sidhesh Dudhabaware <sidheshdudhabaware21916550@gmail.com>
2026-03-11 19:44:19 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import dill
 import numpy as np
 import pandas as pd
 
+from sklearn.metrics import r2_score
 
 def save_object(file_path,obj):
     try:
@@ -12,5 +13,26 @@ def save_object(file_path,obj):
         os.makedirs(dir_path,exist_ok=True)
         with open(file_path,"wb") as file_obj:
             dill.dump(obj,file_obj)
+    except Exception as e:
+        print(e)
+
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for name, model in models.items():
+
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[name] = test_model_score
+
+        return report
+
     except Exception as e:
         print(e)
