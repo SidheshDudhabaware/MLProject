e08e960920813f5edbd92253fe23016e65fda00e
Sidhesh Dudhabaware <sidheshdudhabaware21916550@gmail.com>
2026-03-12 00:18:25 +0530

    Web work done

--- src/utils.py
+++ src/utils.py
@@ -36,3 +36,10 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
 
     except Exception as e:
         print(e)
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        print(e)
